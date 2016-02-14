c2bf32101f3364e96477c774afac141d9de343c7
joe <joe@joeegan.co.uk>
2016-02-14 21:07:20 +0000

    use simplified market signature

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from './table';
-import Market from 'market-faker';
+import { Market } from 'market-faker';
 import css from './styles.css';
 
-window.data = [
-   new Market('Wall Street', 16330.6, 16334.4),
-   new Market('FTSE 100', 5893.6, 5898.6),
-   new Market('Germany 30', 9525.1, 9532.1),
-   new Market('EUR/USD', 11104.5, 11106.0),
-   new Market('GBP/USD', 14597.0, 14599.0),
-   new Market('US Light Crude', 3266.5, 3270.5),
-   new Market('Rolls-Royce Holdings PLC', 513.0, 513.4),
+const data = [
+   new Market('Wall Street', 16330.6),
+   new Market('FTSE 100', 5893.6),
+   new Market('Germany 30', 9525.1),
+   new Market('EUR/USD', 11104.5),
+   new Market('GBP/USD', 14597.0),
+   new Market('US Light Crude', 3266.5),
+   new Market('Rolls-Royce Holdings PLC', 513.0),
  ];
 
-(function animloop(){
+(function animloop() {
   requestAnimationFrame(animloop);
   ReactDOM.render(<Table data={data} />, document.getElementById('root'));
 })();
--- src/row.js
+++ src/row.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 
 export default class Row extends Component {
   render() {
-    var marketNodes = this.props.data.map(function(market) {
+    var marketNodes = this.props.data.map(function (market) {
       return (
         <tr key={market.name}>
           <td>
             <span>{market.name}</span>
           </td>
-          <td className='number'>{market.openingLevel}</td>
-          <td className='number red'>{market.sellPrice}</td>
-          <td className='number blue'>{market.buyPrice}</td>
+          <td className='number'>{market.opening}</td>
+          <td className='number red'>{market.sell}</td>
+          <td className='number blue'>{market.buy}</td>
           <td className='number'>{market.updateTime}</td>
           <td className='number'>{market.change}</td>
-          <td className='number'>{market.percentChange}</td>
+          <td className='number'>{market.changePercentage}</td>
           <td className='number'>{market.high}</td>
           <td className='number'>{market.low}</td>
         </tr>
       );
     });
+
     return (
       <tbody>{marketNodes}</tbody>
     );
